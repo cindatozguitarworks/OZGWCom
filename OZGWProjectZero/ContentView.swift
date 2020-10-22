66b4f1de41b714a3ef4db8eb6996df57c2115859
Cynthia Anderson <cind@ozguitarworks.com>
2020-10-22 10:09:28 -0700

    Initial Commit

---
+++ OZGWProjectZero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OZGWProjectZero
+//
+//  Created by Cynthia Anderson on 10/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OZGWProjectZero/OZGWProjectZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  OZGWProjectZeroApp.swift
+//  OZGWProjectZero
+//
+//  Created by Cynthia Anderson on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct OZGWProjectZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
