899e38c26a6f95a788f0250c800d07c56a6053aa
Cynthia Anderson <cind@ozguitarworks.com>
2021-10-15 14:46:09 -0700

    new view added, cleaned up bluetooth

--- OZGWProjectZero/ContentView.swift
+++ OZGWProjectZero/ContentView.swift
@@ -7,182 +7,95 @@
 
 import SwiftUI
 
-
-//struct MyButtonStyle: ButtonStyle {
-//   func makeBody(configuration: Configuration) -> some View {
-//      let pressed = configuration.isPressed
-//      return configuration.label
-//   }
-//
-//      Text(bleManager.buttonZeroState)
-//      Label("", systemImage:
-//               bleManager.buttonZeroisOn ? "circle" : "circle.fill" )
-   
-//}
-
-
 struct ContentView: View {
    
    @ObservedObject var bleManager = BLEManager()
 
-   //Commit this version
-  
     var body: some View {
-      
-
-            
-            VStack (spacing: 10) {
+       
+      VStack (spacing: 10) {
 //MARK:- TITLE
-               HStack {
-                  Label("", systemImage:
-                   bleManager.isSwitchedOn ? "guitars.fill" : "guitars" )
-            Text("Tau-6")
-               .font(.title)
-               .frame(maxWidth: .infinity, alignment: .center)
-               }
-               //MARK:-START SCAN
-                                   //    HStack (spacing: 10){
-                                          
-                                  Button(action: {
-                                     self.bleManager.startScanning()
-                                 //    print(" Start Scanning")
-                                  }){
-                                    Text("Start Scan   ")
-                                 } //Start Button
-                       //     }
-                        //         .font(.headline)
-                                  .font(.body)
-
-//MARK:- LIST PERIPHERAL
-               List(bleManager.peripherals){
-           peripheral in
-                   HStack{
-                Text(peripheral.name)
-                  Spacer()
-                  Text(String(peripheral.rssi))
-               }
-               }  .lineLimit(1)
-               .frame( height: 48) //list
-                
-           //     Spacer()
-        //        Spacer()
-
-               //MARK:- TEXT FIELD
-             //  VStack(alignment: .leading){
-//                  HStack{
-//               Text(" Sent Data Line ")
-//                  .font(.headline)
-//
-//                     TextField("Enter Data Line", text: $bleManager.dataLine,  onEditingChanged:  { (changed) in print ("Dataline in OnEditing Changed -  \(changed)" )
-//                 }) {
-//                 print ("Dataline onCommit")
-//                 }
-//                  .textFieldStyle(RoundedBorderTextFieldStyle())
-//                  .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-//                  .background(Color .blue)
-//                  }//hstack
-//                  HStack{
-//                 Text(" Received Data  ")
-//                  .font(.headline)
-//
-//                     TextField("", text: $bleManager.dataLine,  onEditingChanged:  { (changed) in print ("Dataline in OnEditing Changed -  \(changed)" )
-//                 }) {
-//                 print ("Dataline onCommit")
-//                 }
-//                  .textFieldStyle(RoundedBorderTextFieldStyle())
-//                  .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-//                  .background(Color .blue)
-//                  }//hstack
-//                 // Text(" Converted Data Line: \(bleManager.dataLine)")
-                 // print(bleManager.dataLine.utf8.map{UInt($0)})
-        //       }//vstack
-             //  Spacer()
-               
-//MARK:- RED LIGHT
-            //   VStack {
-                VStack (spacing: 20){
-                  HStack{
-               //   HStack {
-                     Text("RED ")
-                     
-                     Button(action: {
-                        self.bleManager.writetoRedLED()
-                   
-                     }){
-                        Text(bleManager.redLEDState)
-                    
-                      Label("", systemImage:
-                          bleManager.redLEDisOn ? "circle.fill" : "circle" )
-                     }.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                     //red Light
+        HStack{
+      
+         Label("   ", systemImage: bleManager.isSwitchedOn ? "guitars.fill" : "guitars")
            
-                //     }.padding()
-                   
-          
-               //      HStack {
-//MARK:- GREEN LIGHT
-                      Text("GREEN ")
-                      
-                      Button(action: {
-                         self.bleManager.writetoGreenLED()
-                     
-                      }){
-                         Text(bleManager.greenLEDState)
-                       //   Text(bleManager.buttonZeroState)
-                       Label("", systemImage:
-                           bleManager.greenLEDisOn ? "circle.fill" : "circle" )
-                      }.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
-                      //Green Light
-                      
-               //   }.padding() // HStack Green Light
+           Spacer()
+           Spacer()
+           
+           
+           if bleManager.isSwitchedOn {
+              Text("Bluetooth is Switched ON")
+                 .foregroundColor(.green)
+           } else {
+              Text("Bluetooth is Switched OFF")
+              .foregroundColor(.red)
+           }
+           Spacer()
+         }//Hstack
 
-               }//Hstack
-                  .font(.body)
-                   Spacer()
+            Text("Tau-6")
+                 .font(.title)
+                 .frame(maxWidth: .infinity, alignment: .center)
+         
+//MARK:- LIST PERIPHERAL
+         VStack (spacing: 20){
+            Spacer()
+            
+            List(bleManager.peripherals){
+               peripheral in
+               HStack{
+               Text(peripheral.name)
+               Spacer()
+               Text(String(peripheral.rssi))
+            }
+            }.lineLimit(1)
+            .frame(height: 100)
+            
+
+     //    Spacer()
                   
-//MARK:- BUTTON 0
-                  VStack{
-                  HStack{
-               Text(" Data Out ")
-                  .font(.title)
-                  
-               TextField("Enter Data Line", text: $bleManager.dataLine,  onEditingChanged:  { (changed) in print ("Entering Data to Send- changed? \(changed)" )
+//MARK:- WRITE
+     VStack{
+         HStack{
+            Text(" Data Sent ")
+             
+            TextField("Enter Data Line", text: $bleManager.dataLine,  onEditingChanged:  { (changed) in print ("Entering Data to Send- changed? \(changed)" )
                  }) {
                //  print ("Dataline onCommit")
                  }
                   .textFieldStyle(RoundedBorderTextFieldStyle())
                   .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                  .background(Color .blue)
+                  .background(Color .black)
+            Spacer()
                   }//hstack
-                   HStack {
+      
                      
-                     Text("Writing Data   ") //"guitars")
-                       
-                       // MARK: - ** First button
-                     Button(action: {
-                        self.bleManager.startWriting()
-                   
-                     }){
-                        Text(bleManager.buttonZeroState)
-                        Label("", systemImage:
-                                 bleManager.buttonZeroisOn ? "circle" : "circle.fill" )
-                        
-                     }.padding(6.0)
-                     .border(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, width: 1)
-                               
-
-                     }.padding() // HStack But0
-             
-                  }//Vstack
+            // MARK: - ** Write button
+            Button(action: {
+               self.bleManager.startWriting()                }){
+      
+                  if (bleManager.buttonZeroisOn) {
+                  Text(" WRITE ")
+                        .font(.title2)
+                  }else {
+                     Text(" WRITE ")
+                        .font(.title3)
+                        .bold()
+                  }
+                  
+               }.padding(6.0)
+                .border(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, width: 1)
+               
+      
+            }//Vstack
                   
                 Spacer()
                   
                   //MARK:- BUTTON 1
-                VStack {
-                  HStack{
-                 Text(" Data In  ")
-                  .font(.title)
-                  
+            VStack {
+              HStack{
+                 Text(" Data Read  ")
+                
                      TextField("", text: $bleManager.receivedDataLine,  onEditingChanged:  { (changed) in print ("Printing received data - Changed? -  \(changed)" )
                  }) {
                //  print ("Dataline onCommit")
@@ -190,38 +103,64 @@ struct ContentView: View {
                   .textFieldStyle(RoundedBorderTextFieldStyle())
                   .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                   .background(Color .blue)
+                 Spacer()
+                 
                   }//hstack
                   
-            HStack{
+           
                 Text("Reading Data   " )
                // MARK: - ** First button
                Button(action: {
                   self.bleManager.startReading()
              
                }){
-                Text(bleManager.buttonOneState)
-               Label("", systemImage:
-                    bleManager.buttonOneisOn ? "circle" : "circle.fill" )
-                
+                     if (bleManager.buttonOneisOn) {
+                     Text(" READ ")
+                           .font(.title2)
+                     }else {
+                        Text(" READ ")
+                           .font(.title3)
+                           .bold()
+                     }
+                  
                 }.padding(6.0)
-             //  .background(bleManager.buttonOneisOn ? Color.gray : Color.white)
+            
                .border(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, width: 1)
                
-              
-                }.padding()  //HStack But1
-         }//Vstack
+      }//Vstack
                 Spacer()
-                  
+//MARK:- RED LIGHT
+                        HStack{
+                           
+                            Text("RED ")
+                            Button(action: {
+                                    self.bleManager.writetoRedLED()
+                            }){
+                                    Text(bleManager.redLEDState)
+                                    Label("", systemImage:
+                                      bleManager.redLEDisOn ? "circle.fill" : "circle" )
+                              }.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                        
+            //MARK:- GREEN LIGHT
+                            Text("GREEN ")
+                            Button(action: {
+                                    self.bleManager.writetoGreenLED()
+                            }){
+                                   Text(bleManager.greenLEDState)
+                                   Label("", systemImage:
+                                       bleManager.greenLEDisOn ? "circle.fill" : "circle" )
+                                  }.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+                                 
+                           }//Hstack
+                           .font(.body)
+                        Spacer()
 
              .alignmentGuide(.lastTextBaseline) { dimension in 6 }
              .padding()
                 
                 }
-//             }//VSTACK
-            //    .font(.title)
              .frame(maxWidth: .infinity, alignment: .center)
             }//vstack
-//      } //Navigation View
      } //body
  } //View
 
